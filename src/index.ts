71bbb1ed8aaba968008b464ff2bfc56a0d838593
arnold <arnold@jasny.net>
2023-09-02 14:09:06 +0200

    Graceful shutdown. Fixes #3

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,13 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/identifiers', identifierRoutes);
 app.use('/credentials', credentialRoutes);
 
-// Start the server
-app.listen(PORT);
+const server = app.listen(PORT);
 console.log(`Server running at http://localhost:${PORT}/`);
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    console.log('HTTP server closed');
+  });
+});
+  
